8c941f92390aa27e5d5b9506ae236b209f15d98d
Koels <koels@edemntum.com>
2020-09-08 17:58:20 +0100

    update health response

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 
 app.get('/health', (req, res) => {
-  res.send('ok')
+  res.sendStatus(200)
 })
 
 // use body parser so we can get info from POST and/or URL parameters
